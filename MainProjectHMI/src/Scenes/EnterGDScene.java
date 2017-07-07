825f5323e04c09f862b1ef55ab48337ef391cadb
harshit2205 <harshit.arpit@gmail.com>
2017-07-08 01:29:53 +0530

    EnterGDScene created ready to be linked.

--- MainProjectHMI/src/Scenes/EnterGDScene.java
+++ MainProjectHMI/src/Scenes/EnterGDScene.java
@@ -10,6 +10,10 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 
 
 public class EnterGDScene {
@@ -26,29 +30,52 @@ public class EnterGDScene {
 
 
         //topics of G.D :
-        HBox hBox = new HBox();
-        hBox.setAlignment(Pos.CENTER);
-        Label topicsLabel = new Label("Topics for Group Discussion");
-        hBox.setStyle("-fx-background-color: #000000");
-        hBox.getChildren().add(topicsLabel);
+        Text titleText = new Text("Enter your Details");
+        titleText.setFont(Font.font(null, FontWeight.EXTRA_BOLD,30));
+        titleText.setFill(Paint.valueOf("#01a0e4"));
+
+        HBox titleTextHBox = new HBox();
+        titleTextHBox.setAlignment(Pos.CENTER);
+        titleTextHBox.setStyle("-fx-background-color: #000000");
+        titleTextHBox.setPadding(new Insets(0,10,10,10));
+        titleTextHBox.getChildren().add(titleText);
 
         //List View.......
         ListView<String> listview = new ListView();
-        ObservableList<String> list = FXCollections.observableArrayList();
-        list.add("India Now");
-        list.add("more");
-        listview.setEditable(false);
+        listview.setId("topicslist");
+        listview.setEditable(true);
         listview.setPadding(new Insets(0,10,0,10));
-        listview.setItems(list);
+        listview.setItems(fetchList());
+        // stylying inline listView...................
+        listview.setStyle("-fx-background-color: transparent;");
+
 
         // Enter Group Disscussion.
         Button enterButton = new Button("Enter LIVE Group Discussion");
         vBox.setPadding(new Insets(0,10,10,10));
         vBox.setAlignment(Pos.CENTER);
-        vBox.getChildren().addAll(headerBox,hBox,listview,enterButton);
+        vBox.getChildren().addAll(headerBox,titleTextHBox,listview,enterButton);
 
         scene = new Scene(vBox,700,600);
         scene.getStylesheets().add("Themes/Nevike.css");
         return scene;
+    }
+
+    //function to fetch list of topics........................
+    private static ObservableList<String> fetchList(){
+        ObservableList<String> list = FXCollections.observableArrayList();
+        list.add("What is the major cause of intense civil agitation in Kashmir?");
+        list.add("How escalating pollution rate in India can be controlled?");
+        list.add("Live in relations are increasing in India. What is the cause behind it?");
+        list.add("The performance of India in sports has gone down. What can be a possible cause of it?");
+        list.add("What can bring more development in India?");
+        list.add("How criminalization can be checked in India?");
+        list.add("Which plays crucial role in maintaining democracy in India?");
+        list.add("Draught situations have effected which field of India?");
+        list.add("What must be done to improve the education system of India?");
+        list.add("Many bulk data websites have been banned by telecom authorities.");
+        return list;
+
+        //can also be used to fetch list from management
     }
 }
